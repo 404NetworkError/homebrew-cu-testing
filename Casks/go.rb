e940a00cc2733318d9e287e4a29d795cb069fb53
404NetworkError <404NetworkError@users.noreply.github.com>
2020-10-04 11:40:28 -0400

    Add Go 1.15.1 cask

--- Casks/go.rb
+++ Casks/go.rb
@@ -1,6 +1,6 @@
 cask 'go' do
-  version '1.13.1'
-  sha256 'aff91ee21105f918eb7bab8e625a823ea4f0a3d0e2c0c6866d012bc158c78692'
+  version '1.15.1'
+  sha256 '93a4de1cc066d0706d97ebc6b2a08c2e2ba5eb36c52b5846de6452e443572372'
 
   url "https://golang.org/dl/go#{version}.darwin-amd64.pkg"
   name 'Go'
